e60e4aa25fb913db156561c003f81b4723379ca5
lama <leema442@gmail.com>
2025-12-04 14:44:49 +0300

    add admin for contact

--- AstronomyBeyondLearning/main/admin.py
+++ AstronomyBeyondLearning/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ContactMessage
 
 # Register your models here.
+
+
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "phone", "accepted_terms", "created_at")
+    search_fields = ("first_name", "last_name", "email", "phone", "message")
+    list_filter = ("accepted_terms", "created_at")
+
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
